b649fd77b44e89b717dd5d262cb003bc93e1b11f
Paul Armstrong <parmstrong@gitlab.com>
2021-06-26 21:46:06 +0200

    Changed version, updated init.

--- setup.py
+++ setup.py
@@ -66,6 +66,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/paul-armstrong-dev/download_station_api',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
